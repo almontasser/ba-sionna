e362bf62fd47269ca92f05b61899073e1253f213
hend <hend.jnnt@gmail.com>
2025-12-17 19:40:12 +0200

    multi stage train

--- train.sh
+++ train.sh
@@ -1,4 +1,34 @@
 
-python train.py --require_gpu --channel_gen_device gpu \
-    --train_channels_outside_graph 1 -T 16 --epochs 200 --scenarios "UMi"
+# python train.py --require_gpu --channel_gen_device gpu \
+#     --train_channels_outside_graph 1 -T 16 --epochs 200 --scenarios "UMi"
+#     # ,UMa,RMa
+
+# python train.py --require_gpu --channel_gen_device gpu \
+#     --train_channels_outside_graph 1 -T 16 --epochs 10 --scenarios "UMi"
     # ,UMa,RMa
+
+# # Use pre-generated channels -- obselete not working
+# python train.py --require_gpu --channel_gen_device gpu \
+#     --train_channels_outside_graph 1 -T 16 --epochs 100 --scenarios "UMi,UMa,RMa" --channel_dataset data/channels/train 
+
+# Generate channels
+python train.py --require_gpu --channel_gen_device gpu \
+    --train_channels_outside_graph 1 -T 16 --epochs 50 --scenarios "UMi,UMa,RMa" --channel_cache_size 500 
+
+python train.py --require_gpu --channel_gen_device gpu \
+    --train_channels_outside_graph 1 -T 16 --epochs 50 --scenarios "UMi,UMa,RMa" --channel_cache_size 500 
+
+python train.py --require_gpu --channel_gen_device gpu \
+    --train_channels_outside_graph 1 -T 16 --epochs 50 --scenarios "UMi,UMa,RMa" --channel_cache_size 500 
+
+python train.py --require_gpu --channel_gen_device gpu \
+    --train_channels_outside_graph 1 -T 16 --epochs 50 --scenarios "UMi,UMa,RMa" --channel_cache_size 500 
+
+python train.py --require_gpu --channel_gen_device gpu \
+    --train_channels_outside_graph 1 -T 16 --epochs 50 --scenarios "UMi,UMa,RMa" --channel_cache_size 500 
+
+python train.py --require_gpu --channel_gen_device gpu \
+    --train_channels_outside_graph 1 -T 16 --epochs 50 --scenarios "UMi,UMa,RMa" --channel_cache_size 500 
+
+python train.py --require_gpu --channel_gen_device gpu \
+    --train_channels_outside_graph 1 -T 16 --epochs 50 --scenarios "UMi,UMa,RMa" --channel_cache_size 500 
